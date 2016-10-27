17842449deda0bb01f19da5e9358e10756ca3447
Marcelo Gobetti <mwgobetti@gmail.com>
2016-10-27 10:25:51 -0200

    Initial commit

---
+++ CodeChallenge/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Marcelo Gobetti on 10/27/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
