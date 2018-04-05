1ff36c4bb5acd46f464a61edbed40adfacf9b172
Marcelo Gobetti <mwgobetti@gmail.com>
2018-04-05 02:27:43 -0300

    Added search unit test to view model
    Small refactor to make a test piece reusable. Code coverage for the view
    model is now above 83%.

--- CodeChallengeTests/MoviesListViewModelTests.swift
+++ CodeChallengeTests/MoviesListViewModelTests.swift
@@ -15,6 +15,13 @@ class MoviesListViewModelTests: XCTestCase {
     var disposeBag: DisposeBag!
     var scheduler: TestScheduler!
     
+    func expectedResultsCount(for request: TMDB) -> Int {
+        switch request {
+        case .search: return 18
+        case .upcomingMovies: return 20
+        }
+    }
+    
     override func setUp() {
         super.setUp()
         disposeBag = DisposeBag()
@@ -29,24 +36,14 @@ class MoviesListViewModelTests: XCTestCase {
         let searchRequester = Observable<String>.never()
         let debounceTime = 2
         
-        let viewModel = MoviesListViewModel(pageRequester: pageRequester,
-                                            searchRequester: searchRequester,
-                                            debounceTime: RxTimeInterval(debounceTime),
-                                            scheduler: self.scheduler,
-                                            tmdbModel: tmdbModel)
-        
-        let results = scheduler.createObserver(Int.self)
-        
-        scheduler.scheduleAt(0) {
-            viewModel.moviesDriver.map { $0.count }
-                .drive(results).disposed(by: self.disposeBag)
-        }
-        scheduler.start()
-        
+        let events = simulatedMoviesCountEvents(pageRequester: pageRequester,
+                                                searchRequester: searchRequester,
+                                                debounceTime: debounceTime,
+                                                tmdbModel: tmdbModel)
         let expected = [
-            next(debounceTime, 20)
+            next(debounceTime, expectedResultsCount(for: .upcomingMovies(page: 1)))
         ]
-        XCTAssertEqual(results.events, expected)
+        XCTAssertEqual(events, expected)
     }
     
     func testEmptyListIsReturnedOnNetworkError() {
@@ -62,23 +59,91 @@ class MoviesListViewModelTests: XCTestCase {
         let searchRequester = Observable<String>.never()
         let debounceTime = 2
         
-        let viewModel = MoviesListViewModel(pageRequester: pageRequester,
-                                            searchRequester: searchRequester,
-                                            debounceTime: RxTimeInterval(debounceTime),
-                                            scheduler: self.scheduler,
-                                            tmdbModel: tmdbModel)
-        
-        let results = scheduler.createObserver(Int.self)
-        
-        scheduler.scheduleAt(0) {
-            viewModel.moviesDriver.map { $0.count }
-                .drive(results).disposed(by: self.disposeBag)
-        }
-        scheduler.start()
-        
+        let events = simulatedMoviesCountEvents(pageRequester: pageRequester,
+                                                searchRequester: searchRequester,
+                                                debounceTime: debounceTime,
+                                                tmdbModel: tmdbModel)
         let expected = [
             next(debounceTime, 0)
         ]
-        XCTAssertEqual(results.events, expected)
+        XCTAssertEqual(events, expected)
+    }
+    
+    func testSearchResultsReplaceUpcomingMoviesWhenUserSearches() {
+        let tmdbModel = TMDBModel(moviesClosures: MoyaClosures<TMDB>(endpointClosure: MoyaProvider<TMDB>.defaultEndpointMapping,
+                                                                     stubClosure: MoyaProvider<TMDB>.immediatelyStub))
+        
+        let searchTime = 100
+        let pageRequester = Observable<Void>.never()
+        let searchRequester = scheduler.createHotObservable([
+            next(searchTime, "abc")
+            ])
+        let debounceTime = 2
+        XCTAssertGreaterThan(searchTime, debounceTime,
+                             "This test is not meant to work for a searchTime <= debounceTime")
+        
+        let events = simulatedMoviesCountEvents(pageRequester: pageRequester,
+                                                searchRequester: searchRequester.asObservable(),
+                                                debounceTime: debounceTime,
+                                                tmdbModel: tmdbModel)
+        let expected = [
+            next(debounceTime, expectedResultsCount(for: .upcomingMovies(page: 1))),
+            next(searchTime + debounceTime, expectedResultsCount(for: .search(query: "abc", page: 1)))
+        ]
+        XCTAssertEqual(events, expected)
+    }
+    
+    func testUpcomingMoviesReplaceSearchResultsWhenUserClearsQuery() {
+        let tmdbModel = TMDBModel(moviesClosures: MoyaClosures<TMDB>(endpointClosure: MoyaProvider<TMDB>.defaultEndpointMapping,
+                                                                     stubClosure: MoyaProvider<TMDB>.immediatelyStub))
+        
+        let searchTime = 100
+        let searchClearTime = 200
+        let pageRequester = Observable<Void>.never()
+        let searchRequester = scheduler.createHotObservable([
+            next(searchTime, "abc"),
+            next(searchClearTime, "")
+            ])
+        let debounceTime = 2
+        XCTAssertGreaterThan(searchTime, debounceTime,
+                             "This test is not meant to work for a searchTime <= debounceTime")
+        XCTAssertGreaterThan(searchClearTime, searchTime,
+                             "This test is not meant to work for a searchClearTime <= searchTime")
+        
+        let events = simulatedMoviesCountEvents(pageRequester: pageRequester,
+                                                searchRequester: searchRequester.asObservable(),
+                                                debounceTime: debounceTime,
+                                                tmdbModel: tmdbModel)
+        
+        let expectedResultsCount1 = expectedResultsCount(for: .upcomingMovies(page: 1))
+        
+        let expected = [
+            next(debounceTime, expectedResultsCount1),
+            next(searchTime + debounceTime, expectedResultsCount(for: .search(query: "abc", page: 1))),
+            next(searchClearTime + debounceTime, expectedResultsCount1)
+        ]
+        XCTAssertEqual(events, expected)
+    }
+    
+    private func simulatedMoviesCountEvents(pageRequester: Observable<Void>,
+                                            searchRequester: Observable<String>,
+                                            debounceTime: Int,
+                                            tmdbModel: TMDBModel)
+        -> [Recorded<Event<Int>>] {
+            let viewModel = MoviesListViewModel(pageRequester: pageRequester,
+                                                searchRequester: searchRequester,
+                                                debounceTime: RxTimeInterval(debounceTime),
+                                                scheduler: self.scheduler,
+                                                tmdbModel: tmdbModel)
+            
+            let results = scheduler.createObserver(Int.self)
+            
+            scheduler.scheduleAt(0) {
+                viewModel.moviesDriver.map { $0.count }
+                    .drive(results).disposed(by: self.disposeBag)
+            }
+            scheduler.start()
+            
+            return results.events
     }
 }
