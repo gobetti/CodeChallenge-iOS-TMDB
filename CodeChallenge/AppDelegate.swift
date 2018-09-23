98a4db023435b91226ef831f786b13e960ccdacf
Marcelo Gobetti <mwgobetti@gmail.com>
2018-09-23 19:53:44 -0300

    Updated to Swift 4.2

--- CodeChallenge/AppDelegate.swift
+++ CodeChallenge/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         
         if NSClassFromString("XCTest") != nil {
